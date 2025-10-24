4ec03ab1f44e2a9a461c7f67d2c393c1538abece
azimnorqulov <azim.norqulov@icloud.com>
2025-10-24 11:20:44 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+      <Deals></Deals>
+      <Categories></Categories>
+      <Restaurants></Restaurants>
+      <Application></Application>
+      <Service></Service>
+      <About></About>
+      <Statistics></Statistics>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
